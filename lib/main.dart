c7709908f80bb90912345c2fe18750920d17da29
qishuai <644172593@qq.com>
2020-10-14 10:32:49 +0800

    feature: provider 利用缓存技术，实现了只builder 消费数据的组件Consumer(watch是更新所有组件，尽量不用).

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+
 import 'package:counterstatemanagement/counter_model.dart';
+import 'package:counterstatemanagement/custom_test_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -22,11 +24,21 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
+  BuildContext mContext;
 
   final String title;
 
+  _testReadPro(){
+    // 仅仅读数据不会引发builder.
+    int value = mContext.read<CounterModel>().value;
+    print('_testReadPro $value');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('当前 界面  执行了build方法。。。。。。。。。。 ');
+    mContext = context;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
@@ -36,13 +48,20 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              // .. ${context.watch<CounterModel>().value} 当前界面使用它，会导致所用界面都builder
+              'You have pushed the button this many times: ',
             ),
+            InkWell(onTap: _testReadPro,
+            child: CustomTestWidget(),),
             Consumer<CounterModel>(
-              builder: (context, counter, child) => Text(
-                '${counter.value}',
-                style: Theme.of(context).textTheme.display1,
-              ),
+              builder: (context, counter, child) {
+                print('当前 界面  Consumer 执行了。。。。。。。。。。 ');
+
+                return Text(
+                  '${counter.value}',
+                  style: Theme.of(context).textTheme.display1,
+                );
+              } ,
             ),
           ],
         ),
